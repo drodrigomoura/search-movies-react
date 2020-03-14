c16e16ebf70d48793a7d3ed3fdd432aa094d19cd
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-03-14 00:18:08 -0300

    searchform component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.css'
 import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
 
 function App() {
   return (
     <div className="App">
       <Title>Search movies</Title>
+      <div className="SearchForm-wrapper">
+        <SearchForm />
+      </div>
     </div>
   );
 }

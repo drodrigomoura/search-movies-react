d72d5365475ee2d2624fcae908c8c221669d21bd
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-03-14 21:30:33 -0300

    69 mejoras en la busqueda

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import { SearchForm } from './components/SearchForm'
 import { MoviesList } from './components/MoviesList';
 
 class App extends Component {
-  state = { results: [] }
+  state = { usedSearch: false, results: [] }
 
   _handleResults = (results) => {
-    this.setState({ results })
+    this.setState({ results, usedSearch: true })
+  }
+
+  _renderResults() {
+    return this.state.results.length === 0
+      ? <p>Sorry! 😐 Results not found!</p>
+      : <MoviesList movies={this.state.results} />
+
   }
 
   render() {
@@ -19,9 +26,10 @@ class App extends Component {
         <div className="SearchForm-wrapper">
           <SearchForm onResults={this._handleResults} />
         </div>
-        {this.state.results.length === 0
-          ? <p>Sin resultado</p>
-          : <MoviesList movies={this.state.results} />
+        {
+          this.state.usedSearch
+            ? this._renderResults()
+            : <small>Use the form to search a movie</small>
         }
       </div>
     );
